5973e9193907aab6b699cf812a571d5bf8b1ef36
Nalaka Vilegoda <nalakavilegoda@outlook.com>
2023-05-21 13:55:56 +0530

    Add project files.

---
+++ DueDateCalculator/Controllers/DatesCalculatorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DueDateCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatesCalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public DateTime GetEndDate(DateTime startDate, int daysOfComplete)
+        {
+            DateTime endDate = new DateTime(2022, 8, 19);
+            return endDate;
+        }
+    }
+}

7106236cbd090fcb9479afcb04505dc9ec1b0fd7
Nalaka Vilegoda <nalakavilegoda@outlook.com>
2023-05-21 14:07:43 +0530

    added service layer to calculate task due date

---
+++ DueDateCalculator/Services/CalculateDateService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DueDateCalculator.Services
+{
+    public class CalculateDateService
+    {
+        public DateTime CalculateEndDate(DateTime startDate, int workingDays)
+        {
+            string taskStartDate = startDate.ToString("dd/MM/yyyy");
+            DateTime splitedTaskStartDate = DateTime.ParseExact(taskStartDate, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            DateTime modifiedTaskStartDate = new DateTime(splitedTaskStartDate.Year, splitedTaskStartDate.Month, splitedTaskStartDate.Day);
+            List<DateTime> holidayDates = new List<DateTime>
+            {
+                new DateTime(2022, 8, 23)
+            };
+
+            int totalDays = 0;
+            while (workingDays > 0)
+            {
+                modifiedTaskStartDate = modifiedTaskStartDate.AddDays(1);
+                if (modifiedTaskStartDate.DayOfWeek != DayOfWeek.Saturday && modifiedTaskStartDate.DayOfWeek != DayOfWeek.Sunday && !holidayDates.Contains(startDate.Date))
+                {
+                    workingDays--;
+                }
+                totalDays++;
+            }
+            return modifiedTaskStartDate;
+        }
+    }
+}
